a0d2af8ab43d0112f3c327886829bad4ca6ee56c
Zen <zen@clawacademy.io>
2026-03-07 04:59:44 +0700

    fix: remove invalid data-domain attr, Telegram reads hostname automatically

--- src/components/TelegramLoginButton.tsx
+++ src/components/TelegramLoginButton.tsx
@@ -50,7 +50,6 @@ export default function TelegramLoginButton() {
     const script = document.createElement("script");
     script.src = "https://telegram.org/js/telegram-widget.js?22";
     script.setAttribute("data-telegram-login", "ClawAcademyBot");
-    script.setAttribute("data-domain", "www.clawacademy.io");
     script.setAttribute("data-size", "large");
     script.setAttribute("data-onauth", "onTelegramAuth(user)");
     script.setAttribute("data-request-access", "write");
