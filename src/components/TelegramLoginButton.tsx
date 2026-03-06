6ce6cbb5e113f73510f0d64baf55c7712ba42a12
Zen <zen@clawacademy.io>
2026-03-07 04:59:14 +0700

    fix: add data-domain www.clawacademy.io to Telegram Login Widget

--- src/components/TelegramLoginButton.tsx
+++ src/components/TelegramLoginButton.tsx
@@ -50,6 +50,7 @@ export default function TelegramLoginButton() {
     const script = document.createElement("script");
     script.src = "https://telegram.org/js/telegram-widget.js?22";
     script.setAttribute("data-telegram-login", "ClawAcademyBot");
+    script.setAttribute("data-domain", "www.clawacademy.io");
     script.setAttribute("data-size", "large");
     script.setAttribute("data-onauth", "onTelegramAuth(user)");
     script.setAttribute("data-request-access", "write");
